b7cf029060e1820e50b7177ad0e793a8e2784536
Ismael Zhindon <ismael.zhindon.xd@gmail.com>
2022-07-22 20:13:22 -0500

    Taller 25

--- src/main/java/com/uce/edu/demo/ProyectoU2IzApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2IzApplication.java
@@ -1,7 +1,5 @@
 package com.uce.edu.demo;
 
-import java.time.LocalDateTime;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,59 +7,60 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.repository.modelo.CiudadanoTarea;
-import com.uce.edu.demo.repository.modelo.Pasaporte;
-import com.uce.edu.demo.service.ICiudadadoTareaJpaService;
+import com.uce.edu.demo.repository.modelo.onetomany.Habitacion;
+import com.uce.edu.demo.repository.modelo.onetomany.Hotel;
+import com.uce.edu.demo.service.IHabitacionService;
+import com.uce.edu.demo.service.IHotelService;
 
 @SpringBootApplication
-public class ProyectoU2IzApplication implements CommandLineRunner{
+public class ProyectoU2IzApplication implements CommandLineRunner {
 
 	private static final Logger LOG = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2IzApplication.class, args);
 	}
 
 	@Autowired
-	private ICiudadadoTareaJpaService ciudadadoTareaJpaService;
+	private IHotelService hotelService;
 
-	
+	@Autowired
+	private IHabitacionService habitacionService;
+
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
-		
-		//INSERTAR
-		CiudadanoTarea ciudadano = new CiudadanoTarea();
-        ciudadano.setNombre("Pedro");
-        ciudadano.setApellido("Pezantes");
-        ciudadano.setCedula("0302666966");
-        ciudadano.setFechaNacimiento(LocalDateTime.of(1999, 1, 6, 14, 45));
-        
-        Pasaporte pasaporte = new Pasaporte();
-        pasaporte.setNumero("235");
-        pasaporte.setFechaEmision(LocalDateTime.of(2010, 5, 6, 8, 9));
-        pasaporte.setFechaCaducidad(LocalDateTime.of(2025, 2, 9, 2, 1));
- 
-        ciudadano.setPasaporte(pasaporte);
 
-        pasaporte.setCiudadano(ciudadano);
-        
-        this.ciudadadoTareaJpaService.insertar(ciudadano);
-        
-        //BUSCAR
-        CiudadanoTarea ciud = this.ciudadadoTareaJpaService.buscar("1725363969");
-        LOG.info("Se encontro el estudiante: " + ciud);
-        
-        //ACTUALIZAR
-        CiudadanoTarea ciudadano1 = new CiudadanoTarea();
-        ciudadano1.setId(2);
-        ciudadano1.setApellido("Velez");
-        
-        this.ciudadadoTareaJpaService.actualizar(ciudadano1);
-		
-        //ELIMINAR
-        this.ciudadadoTareaJpaService.eliminar("1725363969");
+		Hotel hotel = new Hotel();
+		hotel.setNombre("Transilvania");
+		hotel.setDireccion("Rumania");
+
+		// this.hotelService.insertar(hotel);
+
+		Habitacion habi = new Habitacion();
+		habi.setNumero("A198");
+		habi.setPiso("10");
+		habi.setTipo("Familiar");
+
+		// this.habitacionService.insertar(habi);
+
+		Hotel hotel1 = new Hotel();
+		hotel1.setId(1);
+
+		Habitacion habitacion1 = new Habitacion();
+		habitacion1.setNumero("A1");
+		habitacion1.setPiso("2");
+		habitacion1.setTipo("matrimonial");
+		habitacion1.setHotel(hotel1);
+
+		Habitacion habitacion2 = new Habitacion();
+		habitacion2.setNumero("A2");
+		habitacion2.setTipo("solitario");
+		habitacion2.setPiso("2");
+		habitacion2.setHotel(hotel1);
+
+		this.habitacionService.insertar(habitacion1);
+
 	}
 
 }
